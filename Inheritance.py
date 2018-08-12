8245441af4431a930e07bb2f79a1f1422986a4de
stonedcoder <vraunaq1213@ghmail.com>
2018-08-12 13:12:10 +0530

    inheritance added
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

---
+++ Inheritance.py
@@ -0,0 +1,40 @@
+class Car :
+
+
+    def_init_(self,color = "white",slogan ="DOPE DOPE DOPE SLOGAN"):
+    
+    self.doors =4 
+    self.seats =4
+    self.color =color
+    self.slogan =slogan
+
+    def start(self):
+        print ("Iam  a car and iam starting")
+
+    def acc(self):
+        print("some car starting sound")
+
+    def stop(self):
+        print("Some car stopping sound")
+
+
+
+
+
+class MarutiCar(Car):
+
+    def_init_(self,abs=True,color="black",slogan="music music")
+    super()._init_(color,slogan)
+
+
+    self.seats=5
+    seats.abs= abs 
+
+
+    def honk(slef):
+    print("some honk sound")
+
+
+
+
+
